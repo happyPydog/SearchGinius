b947c0414ad409c4dd6d87e4d9d4afdd6a459b4f
Rammstein-1994 <lanc840930.mg09@nycu.edu.tw>
2023-08-13 01:11:20 +0800

    add typing for chatgpt response

--- schema.py
+++ schema.py
@@ -5,7 +5,8 @@ from typing import Dict, List, TypedDict
 
 class SupportedFileTypes(str, Enum):
     PYTHON = ".py"
-    
+
+
 class SourceCodeData(TypedDict):
     modules: List[str]
     functions_called: List[str]
@@ -13,3 +14,27 @@ class SourceCodeData(TypedDict):
     classes_used: List[str]
     function_codes: Dict[str, str]
     class_codes: Dict[str, str]
+
+
+## OPENAI CHAT COMPLETION RESPONSE TYPE ##
+class ChoiceMessage(TypedDict):
+    role: str
+    content: str
+
+class Choice(TypedDict):
+    index: int
+    message: ChoiceMessage
+    finish_reason: str
+
+class Usage(TypedDict):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+
+class ChatGPTResponse(TypedDict):
+    id: str
+    object: str
+    created: int
+    model: str
+    choices: List[Choice]
+    usage: Usage
